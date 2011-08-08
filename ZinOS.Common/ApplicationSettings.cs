d92cc4825378660d1a0b256ca7eda4a984f55d17
Mehul Ira <mehulira@gmail.com>
2011-08-08 15:09:05 +0100

    first commit

---
+++ ZinOS.Common/ApplicationSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+using System.Configuration;
+
+namespace ZinOS.Common
+{
+    public static class ApplicationSettings
+    {
+        public static dynamic Setting
+        {
+            get;
+            private set;
+        }
+
+        static ApplicationSettings()
+        {
+            Setting = new SettingsBag();
+        }
+
+        private class SettingsBag : DynamicObject
+        {
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                string name = binder.Name;
+
+                result = getSetting(name);
+
+                return true;
+            }
+
+            private object getSetting(string settingName)
+            {
+                return ConfigurationManager.AppSettings[settingName];
+            }
+        }
+    }
+}
---
+++ ZinOS.Common/TypeLocator.cs
@@ -0,0 +1,40 @@
+﻿using Ninject;
+using Ninject.Modules;
+using System.Dynamic;
+using System.Reflection;
+
+namespace ZinOS.Configuration
+{
+    public class TypeLocator
+    {
+        private static TypeLocator _instance;
+        
+        public static TypeLocator GetInstance()
+        {
+            return _instance;
+        }
+
+        static TypeLocator()
+        {
+            _instance = new TypeLocator();
+        }
+        
+        private IKernel _ninjectKernel;
+
+        private TypeLocator()
+        {
+            initKernel();
+        }
+
+        public T GetType<T>()
+        {
+            return _ninjectKernel.Get<T>();
+        }
+
+        private void initKernel()
+        {
+            _ninjectKernel = new StandardKernel();
+            _ninjectKernel.Load(Assembly.Load("ZinOS.Configuration, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null"));
+        }
+    }
+}
